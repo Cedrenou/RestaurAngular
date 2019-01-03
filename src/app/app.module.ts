34e4c36d90fb99ed251f77ab2ce96502eaeff918
Cedric Renouleau <cedricrenouleau@MacBook-Pro-de-Cedric.local>
2019-01-03 13:00:50 +0100

    dashboard works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,26 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MainNavComponent} from './main-nav/main-nav.component';
 import {LayoutModule} from '@angular/cdk/layout';
-import {MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule} from '@angular/material';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule,
+  MatGridListModule,
+  MatMenuModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatDividerModule
+} from '@angular/material';
 import {HomeComponent} from './home/home.component';
 import {CarteComponent} from './carte/carte.component';
 import {ReserveComponent} from './reserve/reserve.component';
 import {ContactComponent} from './contact/contact.component';
-import { MainDashComponent } from './main-dash/main-dash.component';
-import { DishTableComponent } from './dish-table/dish-table.component';
+import {MainDashComponent} from './main-dash/main-dash.component';
+import {DishTableComponent} from './dish-table/dish-table.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent}, // localhost:4200
@@ -42,12 +55,13 @@ const routes: Routes = [
     MatIconModule,
     MatListModule,
     MatCardModule,
-    RouterModule.forRoot(routes),
     MatGridListModule,
     MatMenuModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatDividerModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-dash/main-dash.component.ts
+++ src/app/main-dash/main-dash.component.ts
@@ -1,33 +1,45 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import {Component, OnInit} from '@angular/core';
+import {map} from 'rxjs/operators';
+import {Breakpoints, BreakpointObserver} from '@angular/cdk/layout';
+
+import {QuickLunchService} from '../services/quick-lunch.service';
+import {Food} from '../models/food.interface';
 
 @Component({
   selector: 'app-main-dash',
   templateUrl: './main-dash.component.html',
   styleUrls: ['./main-dash.component.css']
 })
-export class MainDashComponent {
+export class MainDashComponent implements OnInit {
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
+    map(({matches}) => {
       if (matches) {
         return [
-          { title: 'A la carte', cols: 1, rows: 1 },
-          { title: 'Burgers', cols: 1, rows: 1 },
-          { title: 'Galette / Crêpes', cols: 1, rows: 1 },
-          { title: 'Pizza', cols: 1, rows: 1 }
+          {title: 'Burgers', cols: 1, rows: 1},
+          {title: 'Galette / Crêpes', cols: 1, rows: 1},
+          {title: 'Pizza', cols: 1, rows: 1}
         ];
       }
 
       return [
-        { title: 'A la carte', cols: 2, rows: 1 },
-        { title: 'Burgers', cols: 2, rows: 1 },
-        { title: 'Galette / Crêpes', cols: 2, rows: 1 },
-        { title: 'Pizza', cols: 2, rows: 1 }
+        {title: 'Burgers', cols: 2, rows: 1, id: 'burger'},
+        {title: 'Galette / Crêpes', cols: 2, rows: 1, id: 'galette'},
+        {title: 'Pizza', cols: 2, rows: 1, id: 'pizza'}
       ];
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  burgers: Food[];
+  pizzas: Food[];
+  galettes: Food[];
+
+  constructor(private breakpointObserver: BreakpointObserver, private qls: QuickLunchService) {
+  }
+
+  ngOnInit() {
+    this.burgers = this.qls.getBurgers();
+    this.pizzas = this.qls.getPizzas();
+    this.galettes = this.qls.getGalettes();
+  }
 }
