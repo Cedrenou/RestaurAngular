08c54fdcf018e0588eea5be7eeb0cbedbd9239af
Cedric Renouleau <cedricrenouleau@MacBook-Pro-de-Cedric.local>
2019-01-04 09:12:19 +0100

    contactForm et fin

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  numberToDisplay = '';
 
-  constructor() { }
+  standard = {
+    firstName: 'Ben',
+    lastName: 'Glouton',
+    role: 'Accueil',
+    award: '',
+    phoneNumber: '02-45-03-04-05'
+  };
+
+  sommelier = {
+    firstName: 'David',
+    lastName: 'Biraud',
+    role: 'Sommelier',
+    award: 'Meilleur Ouvrier de France 2002',
+    phoneNumber: '02-47-03-04-05'
+  };
+
+  boucher = {
+    firstName: 'Seb',
+    lastName: 'Coirier',
+    role: 'Boucher',
+    award: 'Meilleur Ouvrier de France 2015',
+    phoneNumber: '02-46-03-04-05'
+  };
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  displayBig(event) {
+    console.log(event);
+    this.numberToDisplay = event;
   }
 
 }
